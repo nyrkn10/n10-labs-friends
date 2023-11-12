5b86f75c640ea88c4af14564bda9e7591ca6aa58
nyrkn10 <kevinnyariki9@gmail.com>
2023-11-12 10:27:06 +0300

    Updates

---
+++ precache-manifest.d6ebeca7f2a7ed6a4016ddc92a14cd6b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798f77bfc7171adc2d4c0e38ccc87f67",
+    "url": "/n10-labs-friends/index.html"
+  },
+  {
+    "revision": "ec1e615cacd55c82b55b",
+    "url": "/n10-labs-friends/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "6a0d5025970fbf1dc44c",
+    "url": "/n10-labs-friends/static/css/main.d415df26.chunk.css"
+  },
+  {
+    "revision": "ec1e615cacd55c82b55b",
+    "url": "/n10-labs-friends/static/js/2.a36be8cc.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/n10-labs-friends/static/js/2.a36be8cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d39f045e1aae5433616c",
+    "url": "/n10-labs-friends/static/js/3.dbb1c9bd.chunk.js"
+  },
+  {
+    "revision": "6a0d5025970fbf1dc44c",
+    "url": "/n10-labs-friends/static/js/main.389d6ee6.chunk.js"
+  },
+  {
+    "revision": "338b2abb9c66fc7dfd98",
+    "url": "/n10-labs-friends/static/js/runtime-main.af497d45.js"
+  },
+  {
+    "revision": "5dfa266e129efc821cb5d7bffa1a8762",
+    "url": "/n10-labs-friends/static/media/Taper-Straight-BF654b8bf78178d.5dfa266e.woff"
+  }
+]);
